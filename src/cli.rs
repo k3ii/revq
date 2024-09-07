5a252e61b9a2056de296fd82fafdbd3f337add82
k3ii <jainr@ringier.co.za>
2024-09-07 15:05:25 +0400

    fix(cli): fix error in short("u") - the trait `IntoResettable<char>` is not implemented for `&str`

--- src/cli.rs
+++ src/cli.rs
@@ -3,9 +3,10 @@ use clap::{command, Arg, Command};
 pub fn cli() -> Command {
     command!().arg(
         Arg::new("username")
-        .short("u")
-        .long("username")
-        .value_name("USERNAME")
-        .help("Specify a Github username")
+            .short('u')
+            .long("username")
+            .value_name("USERNAME")
+            .help("Specify a Github username")
+            .value_parser(clap::value_parser!(String)),
     )
 }
