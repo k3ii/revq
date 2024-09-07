cf47ad1fa53b4566d979a08fd652eaecd3a81ab2
k3ii <jainr@ringier.co.za>
2024-09-07 17:45:20 +0400

    feat(main): Add github auth

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 mod cli;
 mod config;
+mod github;
 mod query;
 
-use query::build_query;
-
 use crate::config::Config;
+use crate::github::fetch_prs;
+use crate::query::build_query;
+use octocrab::Octocrab;
 
 fn main() {
     let matches = cli::cli().get_matches();
     let config = Config::load().expect("Failed to load config");
 
     let username = matches.get_one::<String>("username");
-    let _query = build_query(username.map(|x| x.as_str()), &config);
+    let query = build_query(username.map(|x| x.as_str()), &config);
+
+    let octocrab = match Octocrab::builder()
+        .personal_token(config.token.clone())
+        .build()
+    {
+        Ok(client) => client,
+        Err(e) => {
+            eprintln!("Failed to build Octocrab: {}", e);
+            return;
+        }
+    };
+
 }
