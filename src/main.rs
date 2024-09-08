a330fe250842faa4507cad62c83dd95c4741a005
k3ii <jainr@ringier.co.za>
2024-09-08 14:46:13 +0400

    feat(main): Filter PRs and open via browser

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,50 @@
 mod cli;
 mod config;
 mod github;
+mod pr;
 mod query;
 
 use crate::config::Config;
 use crate::github::fetch_prs;
+use crate::pr::{build_pr_list, select_pr};
 use crate::query::build_query;
 use octocrab::Octocrab;
+use webbrowser;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = cli::cli().get_matches();
     let config = Config::load().expect("Failed to load config");
 
-    let username = matches.get_one::<String>("username");
-    let query = build_query(username.map(|x| x.as_str()), &config);
+    let username = matches.get_one::<String>("username").map(|s| s.as_str());
+    let query = build_query(username, &config);
 
-    let octocrab = match Octocrab::builder()
+    let octocrab = Octocrab::builder()
         .personal_token(config.token.clone())
-        .build()
-    {
-        Ok(client) => client,
-        Err(e) => {
-            eprintln!("Failed to build Octocrab: {}", e);
-            return;
-        }
-    };
+        .build()?;
 
-    let response = fetch_prs(&octocrab, &query).await;
+    let response = fetch_prs(&octocrab, &query).await?;
 
-    match response {
-        Ok(json) => {
-            let formatted = serde_json::to_string_pretty(&json).unwrap();
-            println!("response:\n{}", formatted);
+    let pr_list = build_pr_list(&response);
+
+    match select_pr(pr_list) {
+        Some(selected_prs) if !selected_prs.is_empty() => {
+            for pr in selected_prs {
+                let url = pr.url.as_str();
+                if !url.is_empty() {
+                    if let Err(e) = webbrowser::open(url) {
+                        eprintln!("Failed to open URL: {}", e);
+                    }
+                }
+            }
         }
-        Err(e) => {
-            eprint!("Error fetching PRs: {:?}", e);
+        Some(_) => {
+            println!("No PR selected.");
+        }
+        None => {
+            println!("Action aborted, no PR selected.");
         }
     }
+
+    Ok(())
 }
