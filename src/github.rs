e74d43323d80669bc649886092e8ca6677fcb01e
k3ii <jainr@ringier.co.za>
2024-09-11 21:22:13 +0400

    fix(github): Add context to get proper error message

--- src/github.rs
+++ src/github.rs
@@ -1,9 +1,12 @@
+use anyhow::{Context, Result};
 use octocrab::Octocrab;
 use serde_json::Value;
 
-pub async fn fetch_prs(octocrab: &Octocrab, query: &str) -> Result<Value, octocrab::Error> {
+pub async fn fetch_prs(octocrab: &Octocrab, query: &str) -> Result<Value> {
     let response = octocrab
         .graphql::<Value>(&serde_json::json!({"query": query}))
-        .await?;
+        .await
+        .context("Failed to fetch PRs from GitHub")?;
+
     Ok(response)
 }
